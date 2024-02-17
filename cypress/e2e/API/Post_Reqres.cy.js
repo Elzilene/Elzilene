0ea0ac7f4aaec6fee1f84acba03897010245041c
Elzilene <lenesoares.souza@gmail.com>
2024-02-17 13:19:49 -0300

    Automação de Testes API

---
+++ cypress/e2e/API/Post_Reqres.cy.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress"  />
+
+describe('POST Reqres', () => {
+
+    it('Validar CREATE retornando 200', () => {
+        
+        const dados = {
+            "name": "morpheus",
+            "job": "leader"
+        }
+        
+        cy.request({
+            method: 'POST',
+            url: 'https://reqres.in/api/users',
+            body: dados,
+            failOnStatusCode: false
+        }).then  ((response)  => {
+                expect(response.status).to.eq(201)
+        })  
+    })  
+
+})
